65a674e0e4de7a3dd3708c943dca91672fd9c477
JRaether16 <jasondraether@gmail.com>
2020-09-07 22:02:57 -0500

    first commit, part 1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,34 @@
 
 def sum arr
   # YOUR CODE HERE
+  # arr_sum = 0
+  # arr.each { |x|  arr_sum += x} unless arr.empty?
+  # return arr_sum
+  return arr.sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  # two_sum = 0
+  # if arr.length > 1
+  #   two_sum = arr.max(2).sum
+  # elsif arr.length == 1
+  #   two_sum = arr.max()
+  # else
+  # end
+  return arr.max(2).sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.each_with_index do | x_outer, i_outer |
+    arr.each_with_index do | x_inner, i_inner |
+      if ((x_outer + x_inner) == n) and (i_outer != i_inner)
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
