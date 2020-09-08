3a524002c7b04e17db4664bcbac5fc480db84c56
JRaether16 <jasondraether@gmail.com>
2020-09-08 13:09:21 -0500

    everything passes

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -15,7 +15,7 @@ def max_2_sum arr
   # two_sum = 0
   # if arr.length > 1
   #   two_sum = arr.max(2).sum
-  # elsif arr.length == 1
+  # elsif arr.length == 1cd
   #   two_sum = arr.max()
   # else
   # end
@@ -38,18 +38,61 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return s.downcase.start_with?(/[b-df-hj-np-tv-z]/)
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.length == 0
+    return false
+  elsif s.length == 1
+    if s == "0"
+      return true
+    else
+      return false
+    end
+  else
+    if s !~ /[^01]/
+      return s.end_with?("00")
+    else
+      return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError unless isbn.length > 0
+    raise ArgumentError unless price > 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string
+    return new_string = "$%0.2f" % [@price]
+  end
 end
